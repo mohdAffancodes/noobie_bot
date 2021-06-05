7c8f6c907f4bc5754c19c702afe9c8f3a6a55a75
mohdAffancodes <78096552+mohdAffancodes@users.noreply.github.com>
2021-06-05 16:06:58 +0530

    finshed the bot

--- bot.js
+++ bot.js
@@ -1,9 +1,98 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+let joined = false;
+let connection;
 
-client.on("ready", () => {
-   // List servers the bot is connected to
-   console.log("Servers:");
+client.on("ready", () => {});
+
+client.on("message", (receivedMessage) => {
+   // Prevent bot from responding to its own messages
+   if (receivedMessage.author == client.user) {
+      return;
+   }
+
+   if (receivedMessage.content.startsWith("-")) {
+      processCommand(receivedMessage);
+   }
 });
+
+function processCommand(receivedMessage) {
+   let fullCommand = receivedMessage.content.substr(1); // Remove the leading exclamation mark
+
+   if (fullCommand == "join") {
+      joinCommand(receivedMessage);
+   } else if (fullCommand == "help") {
+      helpCommand(receivedMessage);
+   } else if (joined) {
+      playCodes(fullCommand);
+   } else {
+      receivedMessage.reply("Try `-help` or `-join`");
+   }
+}
+
+async function joinCommand(message) {
+   if (!message.guild) return;
+
+   // Only try to join the sender's voice channel if they are in one themselves
+   if (message.member.voice.channel) {
+      connection = await message.member.voice.channel.join();
+      joined = true;
+   } else {
+      message.reply("You need to join a voice channel first!");
+   }
+}
+
+function playCodes(command) {
+   switch (command) {
+      case "BI":
+         connection.play("./assets/bringit.wav");
+         break;
+      case "GG":
+         connection.play("./assets/goodgame.wav");
+         break;
+      case "WP":
+         connection.play("./assets/piece.wav");
+         break;
+      case "NS":
+         connection.play("./assets/niceshot.wav");
+         break;
+      case "HH":
+         connection.play("./assets/hoorah.wav");
+         break;
+      case "GM":
+         connection.play("./assets/gotme.wav");
+         break;
+      case "CB":
+         connection.play("./assets/cmonboy.wav");
+         break;
+      case "CM":
+         connection.play("./assets/coverme.wav");
+         break;
+      case "NN":
+         connection.play("./assets/no.wav");
+         break;
+      case "RU":
+         connection.play("./assets/readyup.wav");
+         break;
+      case "GS":
+         connection.play("./assets/getsome.wav");
+         break;
+      case "LG":
+         connection.play("./assets/yeah.wav");
+         break;
+      case "MO":
+         connection.play("./assets/moveout.wav");
+         break;
+      default:
+         receivedMessage.reply("No such command : `" + fullCommand + "`");
+         break;
+   }
+}
+
+function helpCommand(message) {
+   message.reply(
+      "\nList of commands -\n`-join` - To make the bot join the voice\n`-LG` – lets go,yeah\n`-BI` – bring it\n`-GS` – get some\n`-RU` – ready up\n`-NN` – noooooooooooo\n`-CB` – come on boy\n`-NS` – Nice shot\n`-GG` – Good Game\n`-HH` – A perfect fighting machine\n`-CM` – cover me\n`-NO` – move out\n`-WP` – You wanna piece of me!\n`-GM` – Oh, They got me!"
+   );
+}
 
 client.login("ODUwMzMwMTgxNDQ0MDQyODEz.YLoJsQ.9Dkq66T0_5D0nz7IqCfn1k0kKwk");
