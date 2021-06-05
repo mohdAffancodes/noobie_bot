3c02045123673d91d0e39980b6701fb2cc2d2bd9
mohdAffancodes <78096552+mohdAffancodes@users.noreply.github.com>
2021-06-05 12:54:57 +0530

    first commit

---
+++ bot.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+   // List servers the bot is connected to
+   console.log("Servers:");
+});
+
+client.login("ODUwMzMwMTgxNDQ0MDQyODEz.YLoJsQ.9Dkq66T0_5D0nz7IqCfn1k0kKwk");
